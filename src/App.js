19307e8314480d6f6aea5e77f3bf58f624b7e3a6
LakSK98 <lakshitha199809@gmail.com>
2022-07-03 16:39:48 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import LineChart from './components/LineChart';
 
 function App() {
+
+  //state for store chart data
+  const [state,setState] = useState(null);
+
+  //method for generate raw data to chart data
+  const generateModel = (responseData)=>{
+    let labels=[];
+    let data=[];
+    responseData.map((record)=>{
+      const date = new Date(record.timeStamp*1000).toLocaleString('en-Us',{timeZone:'Asia/Colombo'});
+      labels.push(date);
+      data.push(record.price);
+    });
+    return {
+      labels:labels,
+      datasets:[{
+        label:'BTC Price Changes',
+        data:data,
+        fill:false,
+        borderWidth:1,
+        borderColor:'rgb(75,192,192)',
+        tension:0.1
+      }]
+    };
+  }
+
+  //request to backend for fetch coin data
+  useEffect(()=>{
+    axios.get('https://localhost:5001/api/CoinData')
+      .then((res)=>{
+        setState(generateModel(res.data));
+      }).catch((err)=>{
+        console.log(err);
+      });
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state&&<LineChart chartData={state} textTitle="Changes over time" />}
     </div>
   );
 }
---
+++ src/components/LineChart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart } from 'chart.js/auto';
+
+//component for view of line chart
+const LineChart = ({chartData,textTitle}) => {
+  return (
+    <Line
+        data={chartData} 
+        options={{
+            plugins:{
+                title:{
+                    display:true,
+                    text:textTitle
+                },
+                legend:{
+                    display:true,
+                    position:'bottom'
+                }
+            }
+        }} 
+    />
+  )
+}
+
+export default LineChart;
